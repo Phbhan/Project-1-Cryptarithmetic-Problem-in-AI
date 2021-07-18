5c9a87c2e3fb0bfc050a9cd78a83fb48ef62e87b
Phbhan <phbhanfw@gmail.com>
2021-07-18 09:57:21 +0700

    Tối ưu Binarizer
    Hơi tối ưu thôi

--- Binarizer.py
+++ Binarizer.py
@@ -1,12 +1,31 @@
-def Recursion(k, tmp, dic, index, u):  # Recursion used to initialize array u
+import copy
+
+
+def Recursion(k, tmp, dic, index, u, check, used):  # Recursion used to initialize array u
     if(k == len(index)):
-        tmpp = tmp.copy()
-        u.append(tmpp)
+        u.append(tmp.copy())
         return
 
-    for i in dic[index[k]]:
-        tmp.append(i)
-        Recursion(k+1, tmp, dic, index, u)
+    if check[index[k]] == None:
+        for i in dic[index[k]]:
+            if len(index[k]) == 1:
+                if used[i] == False:
+                    check[index[k]] = i
+                    used[i] = True
+                    tmp.append(i)
+                    Recursion(k+1, tmp, dic, index, u, check, used)
+                    check[index[k]] = None
+                    used[i] = False
+                    tmp.pop()
+            else:
+                check[index[k]] = i
+                tmp.append(i)
+                Recursion(k+1, tmp, dic, index, u, check, used)
+                check[index[k]] = None
+                tmp.pop()
+    else:
+        tmp.append(check[index[k]])
+        Recursion(k+1, tmp, dic, index, u, check, used)
         tmp.pop()
 
 
@@ -14,12 +33,15 @@ def binarize(inp, dic):
     u = [[] for x in range(len(inp[0]))]
     tmp = []
     index = []
-    for j in range(len(inp[0]) - 1, -1, -1):
+    check = {}
+    used = {}
+    for j in range(len(inp[0]) - 1, -1, -1):  # Xét từ cột phải nhất qua trái
         for i in range(len(inp)):
+            check[inp[i][j]] = None
+            for q in range(0, 10):
+                used[q] = False
             index.append(inp[i][j])  # Gather each alphabet
-        Recursion(0, tmp, dic, index, u[j])
+        Recursion(0, tmp, dic, index, u[j], check, used)
         tmp.clear()
         index.clear()
-
     return u
-
